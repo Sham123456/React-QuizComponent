d44c3ce7f4387db40fa3436c963b78b01dd88587
Shamik Mukherjee <shamikmukherjee95@gmail.com>
2018-08-15 23:06:19 +0530

    Completed the sixth component

--- src/QuizQuestions.js
+++ src/QuizQuestions.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestions extends Component{
-    // constructor(props){
-    //     super(props);
-    // }
+    constructor(props){
+        super(props);
+        this.state = {
+            incorrectAnswer : false
+        }
+    }
 
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer){
-            this.props.showNextQuestionHandler()
+            this.props.showNextQuestionHandler();
+            this.setState({incorrectAnswer:false})
+        }else {
+            this.setState({incorrectAnswer: true})
         }
     }
 
@@ -24,6 +30,7 @@ class QuizQuestions extends Component{
                         {this.props.quiz_question.answer_options.map((answer_option, index) => <QuizQuestionButton key={index} button_text={answer_option} clickHandler = {this.handleClick.bind(this)}/>)}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null }
             </main>
         )
     }
